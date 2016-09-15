6b2f962a0c8cf49be0d07c0d22e032fdcf1daeba
meadowsapps <dmeadows@dmeadowserver.com>
2016-09-14 21:56:53 -0400

    Committing progress for the night (9/14/16)

--- src/main/java/com/meadowsapps/pkmn/Nature.java
+++ src/main/java/com/meadowsapps/pkmn/Nature.java
@@ -1,82 +1,37 @@
 package com.meadowsapps.pkmn;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-
 /**
  * Created by dmeadows on 2/12/15
  */
 public enum Nature {
-    ADAMANT,
-    BASHFUL,
-    BOLD,
-    BRAVE,
-    CALM,
-    CAREFUL,
-    DOCILE,
-    GENTLE,
-    HARDY,
-    HASTY,
-    IMPISH,
-    JOLLY,
-    LAX,
-    LONELY,
-    MILD,
-    MODEST,
-    NAIVE,
-    NAUGHTY,
-    QUIET,
-    QUIRKY,
-    RASH,
-    RELAXED,
-    SASSY,
-    SERIOUS,
-    TIMID;
-
-    private static HashMap<Nature, Double[]> modifiers = new HashMap<Nature, Double[]>();
-
-    static {
-        InputStream stream = Nature.class.getClassLoader().getResourceAsStream("modifiers.txt");
-        InputStreamReader streamReader = new InputStreamReader(stream);
-        BufferedReader reader = new BufferedReader(streamReader);
-
-        int counter = 0;
-        String line = "";
-        try {
-            while ((line = reader.readLine()) != null) {
-                Double[] modifiers = new Double[6];
-
-                int stat = 0;
-                String[] mods = line.split(", ");
-                for (String mod : mods) {
-                    modifiers[stat] = Double.parseDouble(mod);
-                    stat++;
-                }
-                Nature.modifiers.put(values()[counter], modifiers);
-                counter++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-    }
-
-    private String name;
-
-    Nature() {
-        name = StringUtils.toCamelCase(name());
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
+    Adamant,
+    Bashful,
+    Bold,
+    Brave,
+    Calm,
+    Careful,
+    Docile,
+    Gentle,
+    Hardy,
+    Hasty,
+    Impish,
+    Jolly,
+    Lax,
+    Lonely,
+    Mild,
+    Modest,
+    Naive,
+    Naughty,
+    Quiet,
+    Quirky,
+    Rash,
+    Relaxed,
+    Sassy,
+    Serious,
+    Timid;
 
     public Double[] getModifiers() {
-        return modifiers.get(this);
+        return DataTable.getNatureTable().getModifier(this);
     }
 
 }
---
+++ src/test/java/DataTableTest.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Dylan on 9/14/16.
+ */
+public class DataTableTest {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
